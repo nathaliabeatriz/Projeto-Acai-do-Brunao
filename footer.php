5f8f36a5ef2a8748ba2e389812db4b4765028e56
Nathália Beatriz <94134475+nathaliabeatriz@users.noreply.github.com>
2022-11-30 16:31:27 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+    <body>
+        <footer>
+            <div id="rodape">
+                <div class="divisao-rodape">
+                    <a href="#" class="logo"><img src="Imagens/logo-projeto.png" alt=""></a>
+                </div>
+                <div class="divisao-rodape">
+                    <h5>Redes sociais</h5>
+                    <ul>
+                        <li class="redes-sociais">
+                            <a href="https://facebook.com/">
+                                <img src="Imagens/Redes sociais/facebook-13.png" alt="Facebook"></a>
+                            <a class="link" href="https://facebook.com/">Facebook</a>
+                        </li>
+                        <li class="redes-sociais">
+                            <a href="https://instagram.com/">
+                                <img src="Imagens/Redes sociais/Instagram.png" alt="Instagram"></a>
+                            <a class="link" style="color: rgb(255, 0, 128);" href="https://instagram.com/">Instagram</a>
+                        </li>
+                        <li class="redes-sociais" style="border: none;">
+                            <a href="https://twitter.com/">
+                                <img src="Imagens/Redes sociais/Twitter.png" alt="Twitter"></a>
+                            <a class="link" href="https://twitter.com/">Twitter</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="divisao-rodape">
+                    <h5>Endereço e Contato</h5>
+                    <ul>
+                        <li>Rua XX, nº 10 - Patrocínio, MG</li>
+                        <li>Telefone: (XX) XXXX XXXX</li>
+                        <li>Email: acai@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="estilo.css">
+    </head>
+<header>
+    <div class="inner-width">
+        <div class="nav-bar" id="nav-bar1">
+            <a href="#" class="logo"><img src="Imagens/logo-projeto.png" alt=""></a>
+        </div>
+        <div class="nav-bar" id="nav-bar2">
+            <nav class="navigation-menu">
+                <a href="#"><i class="fas fa-home home"></i>Home</a>
+                <a href="#"><i class="fas fa-align-left products"></i>Produtos</a>
+                <a href="#"><i class="fab fa-buffer login"></i>Login</a>
+                <a href="#"><i class="fas fa-users register"></i>Cadastro</a>
+                <a href="#"><i class="fas fa-headset contact"></i>Contato</a>
+            </nav>
+        </div>
+    </div>
+</header>
+</html>
